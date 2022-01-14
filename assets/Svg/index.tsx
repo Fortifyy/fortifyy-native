131285087718da2dd2bf523489e6f60b325c66a4
Sanchit Bhatnagar <sanbht23@gmail.com>
2022-01-14 19:09:09 +0530

    Added Community Page UI

--- assets/Svg/index.tsx
+++ assets/Svg/index.tsx
@@ -1,1 +1,6 @@
+export {default as LocalServices} from "./LocalServices";
 export {default as PageNotFound} from "./PageNotFound";
+export {default as People} from "./People";
+export {default as UndrawWarningCyit} from "./UndrawWarningCyit";
+export {default as UserGroupMonochromatic} from "./UserGroupMonochromatic";
+export {default as UserGroupOutline} from "./UserGroupOutline";
--- screens/HomeTabs/Community.tsx
+++ screens/HomeTabs/Community.tsx
@@ -1,26 +1,61 @@
 import React from "react";
-import {StyleSheet, View} from "react-native";
-import {Button, Heading} from "native-base";
+import {Box, Heading, HStack, Image, Pressable, Text, VStack} from "native-base";
 import {MaterialBottomTabScreenProps} from "@react-navigation/material-bottom-tabs";
 import {HomeTabParamList, RootStackParamList} from "../../interface/navigation";
 import {SCREEN_NAMES} from "../../constants";
 import {CompositeScreenProps} from "@react-navigation/native";
 import {NativeStackScreenProps} from "@react-navigation/native-stack";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Dimensions} from "react-native";
+import {t} from "i18n-js";
+import {LocalServices, UserGroupOutline} from "../../assets/Svg";
 
 interface Props extends CompositeScreenProps<MaterialBottomTabScreenProps<HomeTabParamList, SCREEN_NAMES.Community>, NativeStackScreenProps<RootStackParamList>> {
 }
 
-const Community: React.FC<Props> = ({navigation}) => {
+const {width} = Dimensions.get("window");
+
+const Community: React.FC<Props> = () => {
   return (
-    <View style={styles.container}>
-      <Heading>Community</Heading>
-      <Button onPress={() => {
-        navigation.navigate(SCREEN_NAMES.DummyPage);
-      }}> Go To Dummy Page</Button>
-    </View>
+    <SafeAreaView>
+      <Box mx={2}>
+        <Heading>{t("community.title")}</Heading>
+        <VStack space={"md"} mt={5}>
+          <HStack w={width * (9 / 10)} space={"md"}>
+            <Pressable borderRadius="9" bg={"blue.50"} shadow={"3"} p={2} w={"50%"}
+                       _pressed={{bg: "blue.900"}} alignItems={"flex-start"}>
+              <UserGroupOutline scaleY={3} scaleX={3} translateX={20} />
+              <Heading size={"md"}>{t("community.residents.heading")}</Heading>
+              <Text>Lorem ipsum dolor sit amet, blanditiis odit suscipit!</Text>
+            </Pressable>
+            <Pressable borderRadius="9" bg={"blue.50"} shadow={"3"} p={2} w={"50%"}
+                       _pressed={{bg: "blue.900"}}>
+              <LocalServices scaleY={0.7} scaleX={0.7} />
+              <Heading size={"md"}>{t("community.daily-Help.heading")}</Heading>
+              <Text>Lorem ipsum dolor sit amet, blanditiis odit suscipit!</Text>
+            </Pressable>
+          </HStack>
+          <HStack w={width * (9 / 10)} space={"md"}>
+            <Pressable borderRadius="9" bg={"blue.50"} shadow={"3"} p={2} w={"50%"}
+                       _pressed={{bg: "blue.900"}}>
+              <Image
+                source={{uri: "https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/64/000000/external-emergency-alert-icongeek26-linear-colour-icongeek26.png"}}
+                alt="emergency"
+                size={"sm"} />
+              <Heading size={"md"}>{t("community.emergency.heading")}</Heading>
+              <Text>Lorem ipsum dolor sit amet, blanditiis odit suscipit!</Text>
+            </Pressable>
+            <Pressable borderRadius="9" bg={"blue.50"} shadow={"3"} p={2} w={"50%"}
+                       _pressed={{bg: "blue.900"}}>
+              <Image source={{uri: "https://img.icons8.com/color/48/000000/noticeboard.png"}} size={"sm"}
+                     alt="notice" />
+              <Heading size={"md"}>{t("community.notices.heading")}</Heading>
+              <Text>Lorem ipsum dolor sit amet, blanditiis odit suscipit!</Text>
+            </Pressable>
+          </HStack>
+        </VStack>
+      </Box>
+    </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  container: {},
-});
 export default Community;
